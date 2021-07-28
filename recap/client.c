389654d5189f24ccb19fee42a1b8dc701543c104
dimy20 <enzostrindade16@outlook.com>
2021-07-27 22:02:52 -0300

    simple client

--- recap/client.c
+++ recap/client.c
@@ -1,34 +1,76 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
-int main(){
-    int network_socket = socket(AF_INET,SOCK_STREAM,0);
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(5000);
-    server_address.sin_addr.s_addr = INADDR_ANY; 
-    
-    int err = connect(network_socket,(struct sockaddr *)&server_address,sizeof(server_address));
-    if(err == -1){
-         perror("something went wrong");
+#define PORT "3490"
+#define BUFFSIZE 100
+
+void *get_in_addr(struct sockaddr *sa){
+    if(sa->sa_family == AF_INET){
+       return &(((struct sockaddr_in *)sa)->sin_addr);
     }
-    char buff[256];
-    char test_buff[256]= "this is a test";
-    send(network_socket,&test_buff,sizeof(test_buff),0);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
+int main(int argc, char **argv){
+    int fd, numbytes;    
+    char buff[BUFFSIZE];
+    struct addrinfo hints, *server_info;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+    if (argc != 2) {
+	    fprintf(stderr,"usage: client hostname\n");
+	    exit(1);
+	};
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    
+    if(rv = getaddrinfo(argv[1],PORT,&hints,&server_info) != 0){
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+    };
 
-    recv(network_socket,&buff,sizeof(buff),0);
-    printf("sever sent : %s",buff);
+    struct addrinfo *p;
+    for(p = server_info; p != NULL; p=p->ai_next){
+        if((fd = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) == -1){
+            perror("client: socket");
+            continue;
+        }
+        if(connect(fd,p->ai_addr,p->ai_addrlen) == -1){
+            perror("client : connect");
+            close(fd);
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
 
+    void *addr = (struct sockaddr_in *)get_in_addr((struct sockaddr *)p->ai_addr);
+    inet_ntop(p->ai_family,addr,s,sizeof(s));
+    printf("client: connecting to %s \n",s);
+    freeaddrinfo(server_info);
 
+    if ((numbytes = recv(fd, buff, BUFFSIZE-1, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	}
+    buff[BUFFSIZE] = '\0';
+    printf("client: received '%s'\n",buff);
+	close(fd);
+	return 0;
 
-
-    close(network_socket);
-    return 0;
 }
 
 
