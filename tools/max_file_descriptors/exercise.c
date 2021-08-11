e216d4661335f568dff6d0f090f83c4f3e602765
dimy20 <enzostrindade16@outlook.com>
2021-08-11 03:07:39 -0300

    test passed

--- tools/max_file_descriptors/exercise.c
+++ tools/max_file_descriptors/exercise.c
@@ -25,28 +25,41 @@ void clear_file(char * filename){
         ** --End example --**
 
         ** - Incorret output example -**
+
             word -> line
             new_word -> longer_new_line 
-            
+
             original : This is a random line to illustrate
             Output   : This is a random Longer_new_lineate
+            Output(Correct): This is a random Longer_new_line to illustrate
 
         ** - End incorret output example -**
-        
+
     Scenarios:
         len(word) == len(new_word)
         len(word) > len(new_word)
         len(word) < len(new_word)
+    
+    Offset of adjacet text data is : len(new_word) - len(word) +1;
+            
+    Check if occurences is > 0:
+        Copy chunks of text into a buffer
+
+    else: return 0
     X-X-X-X-X-X-X-[W-O-R-D]X-X-X-X-X-X-X
 */
-int freplace_word(char * filename, char * word, char * new_word){
+
+// add a reverse version? a functionallity that basically undoes the last replacement
+int file_replace_word(char * filename, char * word, char * new_word){
     int w_len= strlen(word);
     int nw_len = strlen(new_word);
     int fd,bytes;
     char word_buff[256];
-    char test[32];
+    char test[256];
+    char tmp_buffer[256];
 
     memset(word_buff,0,sizeof(word_buff));
+    memset(tmp_buffer,0,sizeof(tmp_buffer));
     char c;
     fd = open(filename,O_RDWR);
     int i =0;
@@ -59,9 +72,33 @@ int freplace_word(char * filename, char * word, char * new_word){
                 /*12 123 2 12 31 2 32 12 [12 32] 12 12 X X X X X  */
                 if(strcmp(word_buff,word) == 0){
                     // set the proper offset
-                    off_t offset =  -1 * (strlen(word_buff) + 1);
-                    lseek(fd,offset,SEEK_CUR);
+                    off_t r_offset =  -1 * (strlen(word_buff) + 1);
+                    //lseek(fd,offset,SEEK_CUR);
+
+                    // save last offset
+                    off_t last_offset = lseek(fd,0,SEEK_CUR);
+                    // get end offset
+                    off_t end_offset  = lseek(fd,0,SEEK_END);
+                    // adjacent data that is not to be modified
+                    ssize_t n = end_offset - last_offset;
+                    lseek(fd,last_offset-1,SEEK_SET);
+
+
+                    read(fd,tmp_buffer,n);
+                    //reset
+                    lseek(fd,last_offset,SEEK_SET);
+                    // go back len of word
+                    lseek(fd,r_offset,SEEK_CUR);
+
                     write(fd,new_word,nw_len);
+                    write(fd,tmp_buffer,sizeof(tmp_buffer));
+
+
+                    lseek(fd,last_offset,SEEK_SET);
+                    // save offset 
+                    // add to temp buffer
+                    // go back to old offser
+
 
                     count++;
 
@@ -87,18 +124,7 @@ int freplace_word(char * filename, char * word, char * new_word){
 
 }
 int main(){
-    char * data= "i once was a little fat boy, but now ive changed, im gay now";
-    char c = 0;
-
-/*     int fd = open("file2.txt",O_RDWR);
-    lseek(fd,(off_t)10,SEEK_SET);
-    read(fd,&c,1);
-    printf("%c",c);
-    fflush(stdout);
- */
-
-
-    printf("%d", freplace_word("file2.txt","teta","tetasss"));
+    printf("%d", file_replace_word("demo.txt","asdasdadasdasdasdasdasdasd","x"));
     fflush(stdout);
     return 0;
 
