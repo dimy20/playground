a33c8fd2d7ca281ee4d1820a505f038b59e753b5
dimy20 <enzostrindade16@outlook.com>
2021-08-12 01:08:38 -0300

    args

--- tools/max_file_descriptors/exercise.c
+++ tools/max_file_descriptors/exercise.c
@@ -73,12 +73,25 @@ int read_file(char * filename, char * buff, size_t size){
 
 }
 
-void buff_replace_word(char * src, int size, char * word, char * new_word){
+int write_file(char * filename, char * buff, size_t size){
+    int fd, n_written;
+    int total_written = 0;
+    char *p = buff;
+    if((fd = open(filename,O_WRONLY)) == -1){
+        return -1;
+    }
+    while((n_written = write(fd,p + total_written ,size - total_written)) > 0){
+        total_written += n_written;
+    }
+    (void)close(fd);
+    return ((n_written < 0) ? n_written: total_written);        
+}
+
+int buff_replace_word(char * src, int size, char * word, char * new_word){
     char word_placeholder[WORD_SIZE_MAX];
-    char temp_buffer[256];
-
+    char temp_buffer[MAX_BUFF];
     unsigned int len_new_word = strlen(new_word);
-
+    int count = 0;
     int j = 0;
     unsigned int r_offset = 0;
     for(int i = 0; i<size;i++){
@@ -101,31 +114,105 @@ void buff_replace_word(char * src, int size, char * word, char * new_word){
 
                     //clear temp_buffer for next iteration
                     memset(temp_buffer,0,strlen(temp_buffer));
+                    count++;
             }
             memset(word_placeholder,0,sizeof(word_placeholder));
             j = 0;
         }
     }
     src[strlen(src) - 1] = '\0';
+    return count;
+}
+int split_words(char * arg, char * word, char * new_word){
+    int count  = 0;
+    char * p =NULL;
+    p = strtok(arg," ");
+    strcpy(word,p);
+    while(p != NULL){
+        count ++;
+        strcpy(new_word,p);
+        p = strtok(NULL," ");
+    };
+    return count;
 }
 
+
 int main(int argc, char ** argv){
-    char buff[MAX_BUFF];
-    memset(buff,0,MAX_BUFF);
-    char * filename = argv[1];
-    if(read_file(filename,buff,MAX_BUFF) == -1){
+    if(argc < 2){
+        fprintf(stderr,"Usage : ray [args]");
+        return 1;
+    }
+    char f_flag = 0;
+    char r_flag = 0;    
+    char inv_flag = 0;    
+
+    int option;
+    char * arg = NULL;
+
+    char word[WORD_SIZE_MAX];
+    char new_word[WORD_SIZE_MAX];
+
+    char read_buff[MAX_BUFF];
+    char write_buff[MAX_BUFF];
+    memset(read_buff,0,MAX_BUFF);
+    memset(write_buff,0,MAX_BUFF);
+    char * filename = NULL;
+
+    /* Capture args */
+    while((option = getopt(argc,argv,"f:r:")) != -1){
+            switch(option){
+                case 'f':
+                    filename = optarg;
+                    f_flag = 1;
+                    break;
+                case 'r':
+                     arg = optarg;
+                     r_flag = 1;
+                     break;
+                case '?':
+                    fprintf(stderr,"Usage : ray [args] \n");
+                    break;
+                    return 1;
+                default:
+                    fprintf(stderr,"Usage : ray [args] \n");
+
+            }
+    }
+
+    if(f_flag && r_flag){
+        if(split_words(arg ,word,new_word) != 2){
+            fprintf(stderr,"Invalid word count\n");
+            return 1;
+        }
+    }else{
+        fprintf(stderr,"Missing arguments \n");
+        return 1;
+    }
+
+    if(read_file(filename,read_buff,MAX_BUFF) == -1){
         fprintf(stderr,"Error on file : '%s', error : %s (errno : %d) \n",filename,strerror(errno),errno);
         return 1;
     };
+    /*Could use the same buffer, read to it from file, modify it
+     and then write it back to the file but in case anyting goes wrong
+     i would like to keep a copy of the original data, if an error occurs
+     i just write back the origina data to the file and it would be like nothing
+     ever happened.
+    */
+    strcpy(write_buff,read_buff);
 
-    buff_replace_word(buff,strlen(buff),"test","computer_word");
-    printf("%s \n",buff);
-
-/*     for(int i = 0; i<strlen(buff);i++){
-        printf("%d ",buff[i]);
+    if(buff_replace_word(write_buff,strlen(write_buff),word,new_word) == 0){
+        printf("could not find '%s' on the file, '%s' not modified.",word,filename);
+        return 0;
     }
-        printf("\n");
-    printf("%s \n",buff); */
+
+
+    if(write_file(filename,write_buff,MAX_BUFF) == -1){
+        fprintf(stderr,"Error on file : '%s', error : %s (errno : %d) \n",filename,strerror(errno),errno);
+        // write original data back
+        write_file(filename,read_buff,MAX_BUFF);
+        return 1;
+    };
     return 0;
 
 }
